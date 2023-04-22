67232090f0d81d20ff2ff01f7f1fd5a8246720f6
Blabbermouth35 <poyrazmen35@gmail.com>
2023-04-22 23:53:17 +0300

    Ploted!!!

--- Deposotory/Going_through_walls.py
+++ Deposotory/Going_through_walls.py
@@ -1,6 +1,10 @@
 # You have a 69% chance to run through walls, if you fail, the % Drops by 0.5%, if you do it, the % Rises by 1%.
 
 import random
+import matplotlib.pyplot as plt
+
+x = []
+y = [69.0, ]
 
 
 def run():
@@ -9,13 +13,29 @@ def run():
     while percentage < 1000:
         if random.randint(1, 100) * 10 > percentage:
             percentage -= 5
+            y.append(percentage / 10)
         else:
             percentage += 10
+            if percentage < 1005:
+                y.append(percentage / 10)
         if percentage > 1000:
             print(100)
-            break
+            y.append(100.0)
         else:
             print(percentage / 10)
 
 
 run()
+
+for i in range(len(y)):
+    x.append(i)
+
+print(len(x))
+print(len(y))
+
+plt.xlabel('Tries')
+plt.ylabel('Result Percentage')
+plt.title('You have a 69% chance to go through walls, if you fail, it Drops by 0.5%, if you do it, it Rises by 1%')
+plt.step(x, y)
+plt.grid(True)
+plt.show()
