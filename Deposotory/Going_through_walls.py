ef6d10ed641a09194ff0a36f7b264db66a8a20cb
Blabbermouth35 <poyrazmen35@gmail.com>
2023-04-22 22:58:04 +0300

    A

---
+++ Deposotory/Going_through_walls.py
@@ -0,0 +1,17 @@
+# You have a 69% chance to run through walls, if you fail, the % Drops by 0.5%, if you do it, the % Rises by 1%.
+
+import random
+
+
+def run():
+    percentage = 690
+    print(percentage / 10)
+    while percentage < 1000:
+        if random.randint(1, 100) * 10 > percentage:
+            percentage -= 5
+        else:
+            percentage += 10
+        print(percentage / 10)
+
+
+run()
