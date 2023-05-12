9f4b0e3c8637f62fdca5bd89b5a0ae101e73e83d
Blabbermouth35 <poyrazmen35@gmail.com>
2023-05-12 19:44:30 +0300

    GUİ

--- Deposotory/gelişmiş hesap.py
+++ Deposotory/gelişmiş hesap.py
@@ -10,6 +10,7 @@ def calculating():
 result = ops[selected_operator](first_num, second_num)
 """
 window = tk.Tk()
+window.title("Calculator")
 frame = tk.Frame(master=window, height=200, width=200)
 frame.pack(fill=tk.BOTH, expand=True)
 written_title = tk.Label(master=frame, text="CALCULATOR")
