793c3be5d9dd2d16021674f681cfc497ea96f384
Your Name <poyrazmen35@gmail.com>
2024-05-03 23:47:21 +0300

    TODO: Do more testing, expend the dictionary

--- Deposotory/IPA_text_to_Speech/IPA_text_to_speech.py
+++ Deposotory/IPA_text_to_Speech/IPA_text_to_speech.py
@@ -1,6 +1,9 @@
 import json
 import pygame
 import time
+
+# TODO:
+#    Do more testing, expend the dictionary
 if __name__ == '__main__':
     def get_word_list():
         with open('en_UK.json', 'r', encoding='utf-8') as data:
@@ -11,35 +14,43 @@ if __name__ == '__main__':
         with open("ipa_sounds.json", 'r', encoding='utf=8') as data:
             return json.load(data)
 
+    def process(word_, word_dict_, ipa_sounds_):
+        remove_char = ['ˈ', '/', 'ˌ', "\u200d", ",", ".", "?", "...", "!", "'", ":", ";", "-", "(", ")", '"']
+        sounds = []
+        try:
+            edited_ipa_key = word_dict_[word_]
+        except KeyError:
+            print(f"{word_} isn't in the dictionary")
+            exit()
+        for char in remove_char:
+            edited_ipa_key = edited_ipa_key.replace(char, '')
+        i = 0
+        while i < len(edited_ipa_key):
+            char = edited_ipa_key[i]
+            if i < len(edited_ipa_key) - 1 and char + edited_ipa_key[i + 1] in ipa_sounds_:
+                sounds.append(char + edited_ipa_key[i + 1])
+                i += 2
+            else:
+                sounds.append(char)
+                i += 1
+        print(word_, word_dict_[word_])
+        print(edited_ipa_key)
+        print(sounds)
+        for i in sounds:
+            print(ipa_sounds_.get(i))
+            current_sound = pygame.mixer.Sound(str(ipa_sounds_.get(i)))
+            current_sound.play()
+            time.sleep(1)
+
     pygame.init()
     pygame.mixer.init()
-    remove_char = ['ˈ', '/', 'ˌ', "\u200d"]
-    sounds = []
     ipa_sounds = get_ipa_list()
     word_dict = get_word_list()
-    x = input("Input any word in the English language:")
-    x = x.lower()
-    try:
-        edited_ipa_key = word_dict[x]
-    except KeyError:
-        print(f"{x} isn't in the dictionary")
-        exit()
-    for char in remove_char:
-        edited_ipa_key = edited_ipa_key.replace(char, '')
-    i = 0
-    while i < len(edited_ipa_key):
-        char = edited_ipa_key[i]
-        if i < len(edited_ipa_key) - 1 and char + edited_ipa_key[i + 1] in ipa_sounds:
-            sounds.append(char + edited_ipa_key[i + 1])
-            i += 2
-        else:
-            sounds.append(char)
-            i += 1
-    print(x, word_dict[x])
-    print(edited_ipa_key)
-    print(sounds)
-    for i in sounds:
-        print(ipa_sounds.get(i))
-        current_sound = pygame.mixer.Sound(str(ipa_sounds.get(i)))
-        current_sound.play()
-        time.sleep(1)
+    print("Without any special characters, number symbols, abbreviations etc,")
+    sentence = input("Input any sentence in the English language :")
+    sentence = sentence.lower()
+    words = sentence.split()
+    for word in words:
+        if word == "i":
+            word = "I"
+        process(word, word_dict, ipa_sounds)
